33a8f5877c7f165d61bc262a22ae11ac468946b8
alex.huang <29367395@qq.com>
2022-01-29 22:59:05 +0800

    init

--- LimCore.SocketServer/Program.cs
+++ LimCore.SocketServer/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// https://www.cnblogs.com/gaochundong/p/csharp_tcp_service_models.html
+using LimCore.SocketServer;
+
 Console.WriteLine("Hello, World!");
+TcpSocketRioServer server = new TcpSocketRioServer();
